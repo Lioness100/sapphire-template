06b57a3e702b628d206e9585cd4fe430ef992179
Lioness100 <jchickmm2@gmail.com>
2021-07-26 18:07:24 -0400

    Command precondition shorthands

--- src/lib/structures/Command.ts
+++ src/lib/structures/Command.ts
@@ -2,18 +2,23 @@ import type {
   ArgType,
   CommandOptions as BaseCommandOptions,
   PieceContext,
+  PreconditionEntryResolvable,
 } from '@sapphire/framework';
-import { Command as BaseCommand } from '@sapphire/framework';
+import { Command as BaseCommand, PermissionsPrecondition } from '@sapphire/framework';
 import { toTitleCase } from '@sapphire/utilities';
+import type { PermissionString } from 'discord.js';
 import { sep } from 'path';
+import { Preconditions } from '#types/Enums';
 
 export abstract class Command extends BaseCommand {
   public category: string;
   public usage?: string;
+  public cooldown?: number;
 
   public constructor(context: PieceContext, options: CommandOptions) {
-    super(context, options);
+    super(context, Command.resolvePreconditions(options));
     this.usage = options.usage;
+    this.cooldown = options.cooldown;
     this.category = toTitleCase(this.path.split(sep).reverse()[1]);
   }
 
@@ -25,8 +30,31 @@ export abstract class Command extends BaseCommand {
       throw message;
     });
   }
+
+  public static resolvePreconditions(options: CommandOptions) {
+    options.generateDashLessAliases = options.generateDashLessAliases ?? true;
+    options.preconditions = options.preconditions || [];
+
+    const preconditions = (options.preconditions =
+      options.preconditions || []) as PreconditionEntryResolvable[];
+
+    if (options.permissions) {
+      preconditions.push(new PermissionsPrecondition(options.permissions));
+    }
+
+    if (options.cooldown) {
+      preconditions.push({
+        name: Preconditions.Cooldown,
+        context: { delay: options.cooldown },
+      });
+    }
+
+    return options;
+  }
 }
 
 export interface CommandOptions extends BaseCommandOptions {
   usage?: string;
+  cooldown?: number;
+  permissions?: PermissionString;
 }
