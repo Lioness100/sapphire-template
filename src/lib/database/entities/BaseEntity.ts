0021552843975fc735e0aab49807e808eceb06ab
Lioness100 <jchickmm2@gmail.com>
2021-08-20 08:38:39 -0400

    Remove contructors

--- src/lib/database/entities/BaseEntity.ts
+++ src/lib/database/entities/BaseEntity.ts
@@ -1,12 +1,7 @@
 import type { Snowflake } from 'discord.js';
-import type { EntityData } from '@mikro-orm/core';
 import { PrimaryKey } from '@mikro-orm/core';
 
 export default abstract class BaseEntity {
   @PrimaryKey({ type: 'string' })
   public _id!: Snowflake;
-
-  public constructor(data: EntityData<BaseEntity>) {
-    Object.assign(this, data);
-  }
 }
--- src/lib/database/entities/Guild.ts
+++ src/lib/database/entities/Guild.ts
@@ -1,4 +1,3 @@
-import type { EntityData } from '@mikro-orm/core';
 import { Property, Entity } from '@mikro-orm/core';
 import BaseEntity from '#entities/BaseEntity';
 
@@ -6,8 +5,4 @@ import BaseEntity from '#entities/BaseEntity';
 export default class Guild extends BaseEntity {
   @Property()
   public prefix!: string;
-
-  public constructor(data: EntityData<Guild>) {
-    super(data);
-  }
 }
