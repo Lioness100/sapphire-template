68e93037612fd1f5a6a3b040f1c3131ddc1177cc
Lioness100 <jchickmm2@gmail.com>
2022-05-07 16:14:22 -0400

    refactor: :pencil2: overwrite paginated message "wrong user" reply

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,16 @@ import '@sapphire/plugin-logger/register';
 import 'dotenv/config';
 
 import { SapphireClient, ApplicationCommandRegistries, RegisterBehavior, Piece, container } from '@sapphire/framework';
+import { PaginatedMessage } from '@sapphire/discord.js-utilities';
 import { clientOptions } from '#root/config';
 import process from 'node:process';
 
 const client = new SapphireClient(clientOptions);
 
 ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(RegisterBehavior.Overwrite);
+
+// This reply is overridden for a much less passive aggressive tone.
+PaginatedMessage.wrongUserInteractionReply = (user) => `❌ Only ${user} can use these buttons!`;
 
 // Utility - saves a lot of characters. A lot.
 Object.defineProperty(Piece.prototype, 'client', { get: () => container.client });
